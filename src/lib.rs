f5e5f7301c62b1d4257b94a9e2a5bb612b66283a
nikos <nikos@mugsoft.io>
2019-01-10 12:10:51 +0400

    root address

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+const _ADDR: &str = "https://api.sandbox.paypal.com/v1";
+fn _make_endpoint(ep: &str) -> String //{{{
+{
+    let mut _ep = String::from(_ADDR);
+    _ep.push_str(ep);
+    _ep
+} //}}}
